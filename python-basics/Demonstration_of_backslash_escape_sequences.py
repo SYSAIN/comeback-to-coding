582b16932f35bae42dbfdeac74fba4c0f6e886a2
SYSAIN <ysine764@gmail.com>
2025-11-08 23:44:22 +0100

    backslash

---
+++ python-basics/Demonstration_of_backslash_escape_sequences.py
@@ -0,0 +1,7 @@
+# Example of using backslash \ in strings
+
+print("Hello\nWorld")        # \n adds a new line
+print("He said: \"Hello!\"") # \" lets you use quotes inside a string
+print("C:\\Users\\Yassine")  # double \\ prints a single backslash
+print("Use \\ to escape special characters like ; or \" ")
+
